03b35ab735a5a506b0e69cd17c80a8e8bff4f23d
Alonso Leonardo <alonsoleonardo[at]id.uff.br>
2013-07-19 19:48:13 -0300

    Protótipo Botão Ver em Ações do Projeto

---
+++ src/main/java/br/danielcastellani/gerenciadordetarefas/gui/ButtonProjetoVer.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.danielcastellani.gerenciadordetarefas.gui;
+
+import br.danielcastellani.gerenciadordetarefas.contexto.Contexto;
+import br.danielcastellani.gerenciadordetarefas.controle.TelaPrincipalControlador;
+import br.danielcastellani.gerenciadordetarefas.modelo.Projeto;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ButtonProjetoVer extends JButton {
+
+    private Projeto projeto;
+
+    public ButtonProjetoVer(Projeto projeto) {
+        this.projeto = projeto;
+        this.setText("Ver");
+
+        addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                verProjeto(evt);
+            }
+        });
+    }
+    
+    public void verProjeto(java.awt.event.ActionEvent evt) {
+        TelaPrincipalControlador controladorTelaPrincipal = (TelaPrincipalControlador) Contexto.getInstance().get(TelaPrincipalControlador.class.getCanonicalName());
+        controladorTelaPrincipal.verProjeto(projeto);
+    }
+}
